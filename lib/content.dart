7ccc2b48eea01cae96812b31b571f3b268f744a1
Singhcreations <104023709+singhcreations@users.noreply.github.com>
2023-09-02 15:07:01 +0530

    first intial commit with alarm with ui and firebase

---
+++ lib/content.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ContentScreen extends StatefulWidget {
+  const ContentScreen({super.key});
+
+  @override
+  State<ContentScreen> createState() => _ContentScreenState();
+}
+
+class _ContentScreenState extends State<ContentScreen> {
+  @override
+  void initState() {
+    super.initState();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
